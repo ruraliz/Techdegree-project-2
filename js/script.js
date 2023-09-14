1457a5feecc45af431d9718505602abaa5ee773f
Liz Rurangwa <107972391+ruraliz@users.noreply.github.com>
2023-09-13 18:34:45 -0700

    Added addPagination function and event listener for page buttons

--- js/script.js
+++ js/script.js
@@ -20,27 +20,28 @@ This function will create and insert/append the elements needed to display a "pa
 const studentsPerPage= 9
 function showPage (list, page){
    const startIndex = (page * studentsPerPage) - studentsPerPage;
-   const endIndex = (page * studentsPerPage)
+   const endIndex = (page * studentsPerPage) 
    const studentList= document.querySelector(".student-list")
    studentList.innerHTML= '';
-   for (let i=1; i<list.length; i++){
-      if(i>= startIndex && i<+ endIndex){
-         html= `
-         <li class="student-item cf">
-         <div class="student-details">
-           <img class="avatar" src="${list[i].picture.thumbnail}" alt="Photo of ${list[i].first}">
-           <h3>${list[i].first} + ${list[i].lastname}</h3>
-           <span class="email">${list[i].email}</span>
-         </div>
-         <div class="joined-details">
-           <span class="date">"Joined ${list[i].date}"</span>
-         </div>
-       </li>
+   for (let i=0; i< list.length; i++){
+      if(i>= startIndex && i<= endIndex){
+         const html= `
+            <li class="student-item cf">
+               <div class="student-details">
+                  <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+                  <h3>${list[i].name.first} ${list[i].name.last}</h3>    
+                  <span class="email">${list[i].email}</span>
+               </div>
+               <div class="joined-details">
+                  <span class="date">Joined ${list[i].registered.date}</span>
+               </div>
+            </li>
          `;
-         studentList.insertAdjacentElement("beforeend", html)
+         const printStudents = document.createElement("div")
+         printStudents.innerHTML= html
+         studentList.insertAdjacentElement("beforeend", printStudents);
       } 
    }
-
 }
 
 
@@ -48,7 +49,30 @@ function showPage (list, page){
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
-
-
+function addPagination(list){
+   const numberOfButtons= Math.ceil(list.length / studentsPerPage)
+   const linkList= document.querySelector(".link-list")
+   linkList.innerHTML= '';
+   for(let i=1; i<= numberOfButtons; i++ ){
+      const html= `
+      <li> 
+          <button type= "button">${i}</button>
+      </li>
+    `;
+      linkList.insertAdjacentHTML("beforeend", html);
+   }
+   linkList.querySelector("BUTTON").classList.add("active");
+   linkList.addEventListener('click', (e) => {
+   const activeButton= linkList.querySelector(".active")
+   const clickedButton= e.target.closest("button")
+      if(clickedButton) {
+         activeButton.classList.remove("active");
+         clickedButton.classList.add("active")
+         showPage(data, clickedButton.innerHTML);
+      }
+   })
+}
 
 // Call functions
+showPage(data, 1);
+addPagination(data);
